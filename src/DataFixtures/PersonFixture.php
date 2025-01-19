d61ae69d2d61c7a305367d300d8a017f883a3452
unknown <ferhitayssir@gmail.com>
2025-01-19 10:42:51 +0100

    working with fixtures

--- src/DataFixtures/PersonFixture.php
+++ src/DataFixtures/PersonFixture.php
@@ -2,16 +2,25 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Person;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class PersonFixture extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
 
+        $faker = Factory::create('es_ES');
+        for ($i = 0 ; $i<100 ; $i++) {
+            $person = new Person();
+            $person->setFirstName($faker->firstName);
+            $person->setLastname($faker->lastName);
+            $person->setAge($faker->numberBetween(18, 65));
+
+            $manager->persist($person);
+        }
         $manager->flush();
     }
 }
