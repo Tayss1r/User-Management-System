e1f4f062f9580a0c7ceddcbaaa29025cb754a1a7
unknown <ferhitayssir@gmail.com>
2025-01-22 16:43:47 +0100

    working with entities

---
+++ src/Entity/Profile.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProfileRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProfileRepository::class)]
+class Profile
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $url = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $rs = null;
+
+    #[ORM\OneToOne(mappedBy: 'profile', cascade: ['persist', 'remove'])]
+    private ?Person $person = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): static
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getRs(): ?string
+    {
+        return $this->rs;
+    }
+
+    public function setRs(string $rs): static
+    {
+        $this->rs = $rs;
+
+        return $this;
+    }
+
+    public function getPerson(): ?Person
+    {
+        return $this->person;
+    }
+
+    public function setPerson(?Person $person): static
+    {
+        // unset the owning side of the relation if necessary
+        if ($person === null && $this->person !== null) {
+            $this->person->setProfile(null);
+        }
+
+        // set the owning side of the relation if necessary
+        if ($person !== null && $person->getProfile() !== $this) {
+            $person->setProfile($this);
+        }
+
+        $this->person = $person;
+
+        return $this;
+    }
+}
